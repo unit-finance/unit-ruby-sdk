5d315f936ae8d5492943071b14e903999448e70e
nirmaoz <nir.maoz@unit.co>
2022-12-11 17:27:06 +0200

    simplify tests

--- spec/application_resource_spec.rb
+++ spec/application_resource_spec.rb
@@ -4,9 +4,7 @@ require_relative "spec_helper"
 
 RSpec.describe Unit::ApplicationResource do
   it "Can create new instance" do
-    api_url = "https://testApi.com/"
-    token = "testToken"
-    application_resource = described_class.new(api_url, token)
+    application_resource = described_class.new("", "")
     expect(application_resource).not_to be nil
   end
 end
