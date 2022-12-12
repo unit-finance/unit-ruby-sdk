cee8600b9b459f3bd16eab220c29a993814f2517
nirmaoz <nir.maoz@unit.co>
2022-12-12 08:49:26 +0200

    move modules definition to unit_ruby_sdk.rb

--- lib/unit/api_resources/api_resources.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Unit
-  module Resource
-    autoload :ApplicationResource, "unit/api_resources/application_resource"
-    autoload :CustomerResource, "unit/api_resources/customer_resource"
-  end
-end
