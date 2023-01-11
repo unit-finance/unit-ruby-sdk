f1362eabdc14add443876d27663a5177e1ac6edc
stymoshchuk <sol.tymoshchuk@gmail.com>
2023-01-11 09:30:13 +0100

    RB-4 Update ind card request

--- lib/unit/models/card/create_individual_debit_card_request.rb
+++ lib/unit/models/card/create_individual_debit_card_request.rb
@@ -9,13 +9,13 @@ module Unit
 
       # @param account_id [String]
       # @param customer_id [String] - optional
-      # @param shipping_address [String] - optional
+      # @param shipping_address [Address] - optional
       # @param design [String] - optional
       # @param additional_embossed_text [String] - optional
       # @param idempotency_key [String] - optional
       # @param tags [Hash] - optional
       # @param limits [Hash] - optional
-      # @param print_only_business_name [String] - optional
+      # @param print_only_business_name [Boolean] - optional
       def initialize(account_id, customer_id = nil, shipping_address = nil, design = nil, additional_embossed_text = nil,
                      idempotency_key = nil, tags = nil, limits = nil, print_only_business_name = nil)
         @account_id = account_id
