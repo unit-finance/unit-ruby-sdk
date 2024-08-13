9e2c2da63dcd70654b86c0f2415a1af9b1ae98aa
stymoshchuk <sol.tymoshchuk@gmail.com>
2024-08-13 08:42:19 +0200

    RB-remove-trust-app Removal of trust application

--- spec/application_resource_spec.rb
+++ spec/application_resource_spec.rb
@@ -81,34 +81,4 @@ RSpec.describe Unit::Application do
     expect(response).to be_a(Unit::UnitError)
     expect(response.errors[0].title).to eq "Bearer token is invalid or expired"
   end
-
-  describe "Trust Application" do
-    it "Should create a trust application" do
-      response = described_class.create_trust_application(
-        name: "Trust me Inc.",
-        state_of_incorporation: "CA",
-        revocability: "Revocable",
-        source_of_funds: "Salary",
-        tax_id: "123456789",
-        grantor: GRANTOR,
-        trustees: TRUSTEES,
-        beneficiaries: BENEFICIARIES,
-        contact: TRUST_CONTACT,
-        ip: "127.0.0.2",
-        tags: {
-          "userId": "106a75e9-de77-4e25-9561-faffe59d7814"
-        },
-        idempotency_key: "3a1a33be-4e12-4603-9ed0-820922389fjj"
-      )
-      expect(response.data["type"]).to eq "trustApplication"
-    end
-
-    it "Should update a trust application" do
-      response = described_class.update_trust_application(
-        application_id: "1120231",
-        tags: { purpose: "test" }
-      )
-      expect(response.data["type"]).to eq "trustApplication"
-    end
-  end
 end
